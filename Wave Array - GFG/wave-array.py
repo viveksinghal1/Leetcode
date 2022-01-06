e177c125c8b403a49fbfbb402f0389c173d156ca
Vivek Singhal <48623606+viveksinghal1@users.noreply.github.com>
2022-01-06 20:50:13 +0530

    Added solution - LeetHub

---
+++ Wave Array - GFG/wave-array.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+
+
+class Solution:
+    #Complete this function
+    #Function to sort the array into a wave-like array.
+    def convertToWave(self,arr,N):
+        #Your code here
+        for i in range(1, len(arr), 2):
+            temp = arr[i-1]
+            arr[i-1] = arr[i]
+            arr[i] = temp
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        N=int(input())
+        
+        A=[int(x) for x in input().split()]
+        ob=Solution()
+        ob.convertToWave(A,N)
+        for i in A:
+            print(i,end=" ")
+        
+        
+        print()
+        
+       
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
