fc65cbbe9efd3bf7ea7f7f11a4800a63c62b685b
Vivek Singhal <48623606+viveksinghal1@users.noreply.github.com>
2022-01-30 12:43:11 +0530

    Time: 140 ms (66.05%), Space: 15.2 MB (94.41%) - LeetHub

---
+++ 268-missing-number/268-missing-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        res = 0
+        lnt = len(nums)
+        for i in range(lnt):
+            res += ((i+1)-nums[i])
+            
+        return res
