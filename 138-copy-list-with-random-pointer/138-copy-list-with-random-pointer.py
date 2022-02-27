8d437029dad7142da6c36f46c65c7fd6f08d336c
Vivek Singhal <48623606+viveksinghal1@users.noreply.github.com>
2022-02-27 20:40:05 +0530

    Time: 68 ms (16.72%), Space: 14.9 MB (57.97%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -13,31 +13,25 @@ class Solution:
         if head == None:
             return head
         
-        nodes_arr = []
         mappings = {}
         
         temp = head.next
         
         res = Node(head.val)
         temp2 = res
-        nodes_arr.append(res)
-        c = 1
-        mappings[head] = 0
+        mappings[head] = res
         
         while temp != None:
             temp2.next = Node(temp.val)
-            mappings[temp] = c
-            c += 1
-            temp = temp.next
             temp2 = temp2.next
-            nodes_arr.append(temp2)
-            
+            mappings[temp] = temp2
+            temp = temp.next
             
         temp, temp2 = head, res
         
         while temp != None:
             if temp.random != None:
-                temp2.random = nodes_arr[mappings[temp.random]]
+                temp2.random = mappings[temp.random]
             temp = temp.next
             temp2 = temp2.next
             
