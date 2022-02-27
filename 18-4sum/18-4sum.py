f9ae4477e7f23f87dd41bd941c38f163b15fb8bf
Vivek Singhal <48623606+viveksinghal1@users.noreply.github.com>
2022-02-27 21:28:47 +0530

    Time: 867 ms (68.48%), Space: 13.8 MB (97.06%) - LeetHub

---
+++ 18-4sum/18-4sum.py
@@ -0,0 +1,38 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        
+        n = len(nums)
+        
+        nums.sort()
+        i = 0
+        while i < n:
+            target_i = target - nums[i]
+            j = i + 1
+            while j < n:
+                target_j = target_i - nums[j]
+                
+                front, end = j+1, n-1
+                
+                while front < end:
+                    two_sum = nums[front] + nums[end]
+                    
+                    if two_sum < target_j:
+                        front += 1
+                    elif two_sum > target_j:
+                        end -= 1
+                    else:
+                        res.append([nums[i], nums[j], nums[front], nums[end]])
+                        while front < end and nums[front] == res[-1][2]:
+                            front += 1
+                        while front < end and nums[end] == res[-1][3]:
+                            end -= 1
+                            
+                while j + 1 < n and nums[j] == nums[j+1]:
+                    j += 1
+                j += 1
+            while i + 1 < n and nums[i] == nums[i+1]:
+                i += 1
+            i += 1
+                
+        return res
